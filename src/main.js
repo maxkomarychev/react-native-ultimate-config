a48b1936cfa245f88563db3c7dd7f6b942b3c479
Max Komarychev <maxkomarychev@gmail.com>
2020-07-12 23:51:49 +0300

    wip

--- src/main.js
+++ src/main.js
@@ -2,15 +2,7 @@ const load_env = require("./load-env");
 const render_env = require("./render-env");
 const write_env = require("./write-env");
 const flatten = require("./flatten");
-
-async function resolve_env(env, rc) {
-  if (rc && rc.on_env) {
-    const patched_env = await rc.on_env(env);
-    return patched_env || env;
-  } else {
-    return env;
-  }
-}
+const resolve_env = require("./resolve-env");
 
 module.exports = async function (project_root, lib_root, env_file, rc) {
   const env = await resolve_env(load_env(env_file), rc);
