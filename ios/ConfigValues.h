e3bc89655f637fed3b0f8dc6433fceb316e91317
Max Komarychev <maxkomarychev@gmail.com>
2020-07-13 00:08:33 +0300

    wip

--- ios/ConfigValues.h
+++ ios/ConfigValues.h
@@ -1,1 +1,10 @@
-#error "invoke bin.js with env file before compiling native project"
+// DO NOT COMMIT OR EDIT THIS FILE
+#define MY_VARIABLE @"hello"
+#define MY_URL @"http://hello.world?howareyoudoing=ok"
+
+static NSDictionary *getValues() {
+    return @{
+        @"MY_VARIABLE": MY_VARIABLE,
+        @"MY_URL": MY_URL,
+    };
+}
