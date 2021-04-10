fe43d16f33290eb482298f72e11dc5193391bdc4
Max Komarychev <maxkomarychev@gmail.com>
2021-04-10 22:48:28 +0300

    Mock rnuc in tests

--- packages/example/__tests__/App-test.js
+++ packages/example/__tests__/App-test.js
@@ -9,6 +9,10 @@ import App from '../App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+jest.mock('react-native-ultimate-config', () => ({
+  HELLO: 42,
+}));
+
 it('renders correctly', () => {
   renderer.create(<App />);
 });
