3b2f710edf701a32538e375d97136d705efd87ab
Elliot Dickison <elliotdickison@hey.com>
2021-08-31 08:51:51 -0700

    Fix gitignore for test output directory
    The gitignore line to ensure the test_outputs directory was _not_
    ignored was pointing at a folder that did not exist. I updated it to
    point at the correct folder and added the previously ignored test output
    file for index.web.js.

---
+++ packages/react-native-ultimate-config/src/test_outputs/index.web.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "MY_VARIABLE": "hello",
+  "MY_URL": "http://hello.world?howareyoudoing=ok",
+  "MY_STRING": "string_value",
+  "MY_NUMBER": 42,
+  "MY_BOOLEAN": true,
+  "MY_BOOLEAN_FALSE": false,
+  "QUOTES": "hello \"world'",
+  "PER_PLATFORM": "goodbye"
+}
