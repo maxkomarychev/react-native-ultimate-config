105cc03568dc2686679c787c11dd28708679ccfe
Vagnerlandio Nunes <vagnerlandio@gmail.com>
2023-01-19 01:44:08 -0300

    feat: update package to support react-native 0.71

--- packages/react-native-ultimate-config/android/src/main/java/com/reactnativeultimateconfig/UltimateConfigModule.java
+++ packages/react-native-ultimate-config/android/src/main/java/com/reactnativeultimateconfig/UltimateConfigModule.java
@@ -1,43 +1,46 @@
 package com.reactnativeultimateconfig;
 
+import androidx.annotation.NonNull;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
-import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+import com.facebook.react.module.annotations.ReactModule;
 import java.util.*;
 
+import com.reactnativeultimateconfig.UltimateConfigModule;
+
+@ReactModule(name = UltimateConfigModule.NAME)
 public class UltimateConfigModule extends ReactContextBaseJavaModule {
+  public static final String NAME = "UltimateConfig";
 
-    private final ReactApplicationContext reactContext;
+  private static Class _buildConfig;
+  public static void setBuildConfig(Class buildConfig) {
+    _buildConfig = buildConfig;
+  }
 
-    private static Class _buildConfig;
-    public static void setBuildConfig(Class buildConfig) {
-        _buildConfig = buildConfig;
+  public UltimateConfigModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+
+  @Override
+  public Map<String, Object> getConstants() {
+    final Map<String, Object> constants = new HashMap<>();
+    try {
+      Class act = _buildConfig;
+      String keys = (String)act.getField("__RNUC_KEYS").get(act);
+      for (String k: keys.split(",")) {
+        constants.put(k, act.getField(k).get(act));
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
     }
-
-    public UltimateConfigModule(ReactApplicationContext reactContext) {
-        super(reactContext);
-        this.reactContext = reactContext;
-    }
-
-    @Override
-    public String getName() {
-        return "UltimateConfig";
-    }
-
-
-    @Override
-    public Map<String, Object> getConstants() {
-        final Map<String, Object> constants = new HashMap<>();
-        try {
-            Class act = _buildConfig;
-            String keys = (String)act.getField("__RNUC_KEYS").get(act);
-            for (String k: keys.split(",")) {
-                constants.put(k, act.getField(k).get(act));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return constants;
-    }
+    return constants;
+  }
 }
--- packages/react-native-ultimate-config/ios/UltimateConfig.h
+++ packages/react-native-ultimate-config/ios/UltimateConfig.h
@@ -1,5 +1,12 @@
+
+#ifdef RCT_NEW_ARCH_ENABLED
+#import "RNUltimateConfigSpec.h"
+
+@interface UltimateConfig : NSObject <NativeUltimateConfigSpec>
+#else
 #import <React/RCTBridgeModule.h>
 
 @interface UltimateConfig : NSObject <RCTBridgeModule>
+#endif
 
 @end
